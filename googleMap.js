c8af8941aa5dfd201b1d3666356093a835dd5971
Dave Hoffman <spaghettidave80@gmail.com>
2018-09-26 06:16:00 -0400

    Street view is now working in the infowindow, added a drawing tool.

--- googleMap.js
+++ googleMap.js
@@ -1,6 +1,11 @@
 var map;
+
 // Create a new blank array for all the listing markers.
 var markers = [];
+
+// This global polygon variable is to ensure only ONE polygon is rendered
+var polygon = null;
+
 function initMap() {
   // Create a styles array to use with the map.
   var styles = [
@@ -87,16 +92,32 @@ function initMap() {
     {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
   ];
   var largeInfowindow = new google.maps.InfoWindow();
+
+  // Initialize the drawing manager
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_LEFT,
+      drawingModes: [
+        google.maps.drawing.OverlayType.POLYGON
+      ]
+    }
+  });
+
   // Style the markers a bit. This will be our listing marker icon.
   var defaultIcon = makeMarkerIcon('0091ff');
+
   // Create a "highlighted location" marker color for when the user
   // mouses over the marker.
   var highlightedIcon = makeMarkerIcon('FFFF24');
+
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
     var position = locations[i].location;
     var title = locations[i].title;
+
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
       position: position,
@@ -105,12 +126,15 @@ function initMap() {
       icon: defaultIcon,
       id: i
     });
+
     // Push the marker to our array of markers.
     markers.push(marker);
+
     // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
+
     // Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
     marker.addListener('mouseover', function() {
@@ -119,10 +143,45 @@ function initMap() {
     marker.addListener('mouseout', function() {
       this.setIcon(defaultIcon);
     });
+
   }
+
   document.getElementById('show-listings').addEventListener('click', showListings);
   document.getElementById('hide-listings').addEventListener('click', hideListings);
+
+  // This event listener either shows or hides the drawing tools
+  document.getElementById('toggle-drawing').addEventListener('click', function() {
+    toggleDrawing(drawingManager);
+  });
+
+  document.getElementById('zoom-to-area').addEventListener('click', function() {
+    zoomToArea();
+  });
+
+  // Add an event listener so that the polygon is captured, call the
+  // searchWithinPolygon function. This will show the markers in the
+  // polygon, and hide any outside of it.
+  drawingManager.addListener('overlaycomplete', function(event) {
+    // First, check if there is an existing polygon.
+    // If there is, get rid of it and remove the markers
+    if(polygon) {
+      polygon.setMap(null);
+      hideListings(markers);
+    }
+    // Switching the drawing mode to the Hand (i.e., no longer drawing).
+    drawingManager.setDrawingMode(null);
+    // Creating a new editable polygon from the overlay.
+    polygon = event.overlay;
+    polygon.setEditable(true);
+    // Search withing the polygon.
+    searchWithinPolygon();
+    // Make sure the search is re-done if the poly is changed.
+    polygon.getPath().addListener('set_at', searchWithinPolygon);
+    polygon.getPath().addListener('insert_at', searchWithinPolygon);
+  });
+
 }
+
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
@@ -132,18 +191,20 @@ function populateInfoWindow(marker, infowindow) {
     // Clear the infowindow content to give the streetview time to load.
     infowindow.setContent('');
     infowindow.marker = marker;
+
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
+
     var streetViewService = new google.maps.StreetViewService();
     var radius = 50;
+
     // In case the status is OK, which means the pano was found, compute the
     // position of the streetview image, then calculate the heading, then get a
     // panorama from that and set the options
     function getStreetView(data, status) {
       if (status == google.maps.StreetViewStatus.OK) {
-        console.log('status is ' + status);
         var nearStreetViewLocation = data.location.latLng;
         var heading = google.maps.geometry.spherical.computeHeading(
           nearStreetViewLocation, marker.position);
@@ -163,9 +224,11 @@ function populateInfoWindow(marker, infowindow) {
           '<div>No Street View Found</div>');
       }
     }
+
     // Use streetview service to get the closest streetview image within
     // 50 meters of the markers position
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
     // Open the infowindow on the correct marker.
     infowindow.open(map, marker);
   }
@@ -180,12 +243,14 @@ function showListings() {
   }
   map.fitBounds(bounds);
 }
+
 // This function will loop through the listings and hide them all.
 function hideListings() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
 }
+
 // This function takes in a COLOR, and then creates a new marker
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
@@ -198,4 +263,30 @@ function makeMarkerIcon(markerColor) {
     new google.maps.Point(10, 34),
     new google.maps.Size(21,34));
   return markerImage;
+}
+
+// This shows and hides (respectively) the drawing options.
+function toggleDrawing(drawingManager) {
+  if(drawingManager.map) {
+    drawingManager.setMap(null);
+    // In case the user drew anything, get rid of the polygon
+    if(polygon !== null) {
+      polygon.setMap(null);
+    }
+  } else {
+    drawingManager.setMap(map);
+  }
+}
+
+// This function hides all markers outside the polygon,
+// and shows only the ones within it. This is so that the
+// user can specify an exact area of search.
+function searchWithinPolygon() {
+  for(var i = 0; i < markers.length; i++) {
+    if(google.maps.geometry.poly.containsLocation(markers[i].position, polygon)) {
+      markers[i].setMap(map);
+    } else {
+      markers[i].setMap(null);
+    }
+  }
 }
